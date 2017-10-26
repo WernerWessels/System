9c0f5c79c2ba420ffb39af6e83735579e070c8a2
Werner Wessels <werner.wessels.work@gmail.com>
2017-10-27 00:02:40 +0200

    csv

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171026133153) do
+ActiveRecord::Schema.define(version: 20171026195522) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "manager"
+    t.string "status"
+    t.integer "terms"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "quotes", force: :cascade do |t|
     t.datetime "date"
